4ae649f07d1d79f0bf9ebc7f32f3dd03a94246c2
seungah <fofo2525@naver.com>
2017-06-20 19:56:01 +0900

    Event Programming(pause Button)

--- app/src/main/java/starbloom0128/kr/hs/emirim/audiolist/MainActivity.java
+++ app/src/main/java/starbloom0128/kr/hs/emirim/audiolist/MainActivity.java
@@ -13,50 +13,65 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView list;
-    Button butPlay, butStop;
+    Button butPlay,butStop,butPause;
     TextView textMusic;
     ProgressBar progress;
-    String[] musics = {"open","handsonme","superhot"};
-    int[] musicResids = {R.raw.open, R.raw.handsonme, R.raw.superhot};
+    String[] music={"open","handsonme","superhot"};
+    int[] musicResIds={R.raw.open,R.raw.handsonme,R.raw.superhot};
     int selectedMusicId;
     MediaPlayer mediaPlayer;
-
+    boolean selectedPauseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list = (ListView) findViewById(R.id.list_music);
-        butPlay = (Button) findViewById(R.id.but_play);
-        butStop = (Button) findViewById(R.id.but_stop);
-        textMusic = (TextView) findViewById(R.id.text_music);
-        progress = (ProgressBar) findViewById(R.id.progress_music);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, musics);
+        list=(ListView)findViewById(R.id.list_music);
+        butPlay=(Button)findViewById(R.id.but_play);
+        butStop=(Button)findViewById(R.id.but_stop);
+        butPause=(Button)findViewById(R.id.but_pause);
+        textMusic=(TextView)findViewById(R.id.text_music);
+        progress=(ProgressBar)findViewById(R.id.progress_music);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice,music);
         list.setAdapter(adapter);
         list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        list.setItemChecked(0, true);
-        selectedMusicId = musicResids[0];
-        mediaPlayer = MediaPlayer.create(this, selectedMusicId);
+        list.setItemChecked(0,true);
+        selectedMusicId=musicResIds[0];
+        mediaPlayer=MediaPlayer.create(this,selectedMusicId);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() { //항목이 클릭되었을 때 실행
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                selectedMusicId = musicResids[i];
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                selectedMusicId=musicResIds[i];
             }
         });
-
         butPlay.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                mediaPlayer.create(MainActivity.this, selectedMusicId)
+            public void onClick(View v) {
+                if(selectedPauseButton) {
+                    mediaPlayer.start();
+                    selectedPauseButton=false;
+                }
+                else
+                    mediaPlayer=MediaPlayer.create(MainActivity.this,selectedMusicId);
                 mediaPlayer.start();
+                progress.setVisibility(View.VISIBLE);
             }
         });
         butStop.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 mediaPlayer.stop();
+                progress.setVisibility(View.INVISIBLE);
             }
         });
+        butPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+                selectedPauseButton=true;
+            }
+        });
+
     }
 
     @Override
